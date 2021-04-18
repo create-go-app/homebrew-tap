04ebfbba57ba29a1c2779b58a10bb7226476fc31
Vic Shóstak <vikkyshostak@gmail.com>
2021-04-18 18:53:38 +0300

    Update cgapp.rb

--- cgapp.rb
+++ cgapp.rb
@@ -5,21 +5,21 @@
 class Cgapp < Formula
   desc "A powerful CLI for the Create Go App project. Create a new production-ready project with backend, frontend and deploy automation by running one CLI command!"
   homepage "https://create-go.app/"
-  version "v1.7.4"
+  version "v1.7.5"
   license "Apache-2.0"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/create-go-app/cli/releases/download/v1.7.4/cgapp_v1.7.4_macOS_x86_64.tar.gz"
-    sha256 "223f176429cda8384722a6086c3487665159c77b6cf787028f733b8e92faca90"
+    url "https://github.com/create-go-app/cli/releases/download/v1.7.5/cgapp_v1.7.5_macOS_x86_64.tar.gz"
+    sha256 "e8b87032c8345d538083dec588c5a66ffd20e64e375ab990c181afb7e047f754"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/create-go-app/cli/releases/download/v1.7.4/cgapp_v1.7.4_macOS_arm64.tar.gz"
-    sha256 "d55293c261fe655625ac00079de3a4b92734da92f1a3fa373742b194a243f8b6"
+    url "https://github.com/create-go-app/cli/releases/download/v1.7.5/cgapp_v1.7.5_macOS_arm64.tar.gz"
+    sha256 "07c5a20bdd7dde0d5af59b6054b8fcf1aeab6952a01f93d6e44944bdc705ff0d"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/create-go-app/cli/releases/download/v1.7.4/cgapp_v1.7.4_Linux_x86_64.tar.gz"
-    sha256 "915463f12bf221921afc20325a6495a76575600fc340d75909aa9a259e607ac4"
+    url "https://github.com/create-go-app/cli/releases/download/v1.7.5/cgapp_v1.7.5_Linux_x86_64.tar.gz"
+    sha256 "117fdd04a98a3172960aa1d8540563a162b794bd290da514b07da3109a8bde40"
   end
 
   depends_on "git"
